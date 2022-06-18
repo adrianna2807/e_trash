5cf49453044af2c5b92198b901ef66435d96270d
Ada <uzytkownik@kursGIT.pl>
2022-06-18 11:23:20 +0200

    new app trash

--- base/models.py
+++ base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from trash.models import Trash
+
 
 class Zone(models.Model):
     name = models.CharField(max_length=128)
@@ -63,51 +65,4 @@ class Order(models.Model):
         return f"{self.order_number}"
 
 
-class Trash(models.Model):
-    TRASH = ['Electrical Waste', 'Recycled Waste', 'Hazardous Waste', 'Large Size Waste']
-    type = models.CharField(choices=TRASH, max_length=128)
 
-    def __str__(self):
-        return f"{self.type}"
-
-
-class EWaste(models.Model):
-    TRASH = ['dishwasher', 'TV', 'laptop', 'inne']
-    type = models.CharField(choices=TRASH, max_length=128)
-    height = models.PositiveSmallIntegerField(max_length=5)
-    width = models.PositiveSmallIntegerField(max_length=5)
-    length = models.PositiveSmallIntegerField(max_length=5)
-    weight = models.PositiveSmallIntegerField(max_length=5)
-
-    def __str__(self):
-        return f"{self.type}"
-
-
-class RWaste(models.Model):
-    TRASH = ['paper', 'plastic', 'glass', 'bio']
-    type = models.CharField(choices=TRASH, max_length=128)
-    trash_amount = models.PositiveSmallIntegerField(max_length=5)
-
-    def __str__(self):
-        return f"{self.type}"
-
-
-class HWaste(models.Model):
-    TRASH = ['paint', 'oil', 'medicine', 'other']
-    type = models.CharField(choices=TRASH, max_length=128)
-    trash_amount = models.PositiveSmallIntegerField(max_length=5)
-
-    def __str__(self):
-        return f"{self.type}"
-
-
-class LSWaste(models.Model):
-    TRASH = ['furniture', 'rubble', 'other']
-    type = models.CharField(choices=TRASH, max_length=128)
-    height = models.PositiveSmallIntegerField(max_length=5)
-    width = models.PositiveSmallIntegerField(max_length=5)
-    length = models.PositiveSmallIntegerField(max_length=5)
-    weight = models.PositiveSmallIntegerField(max_length=5)
-
-    def __str__(self):
-        return f"{self.type}"
