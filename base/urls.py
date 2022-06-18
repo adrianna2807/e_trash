da64b40e664252131b90bf7278735f34c18aa7b5
Szuwar <marekszubartowicz@gmail.com>
2022-06-18 11:14:32 +0200

    rename base

--- base/urls.py
+++ base/urls.py
@@ -1,4 +1,4 @@
-"""e_trash URL Configuration
+"""base URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
