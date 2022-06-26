63665a5ce7c15f301c68a5f6eb50115280ff33d2
Ada <uzytkownik@kursGIT.pl>
2022-06-26 09:31:05 +0200

    clients views

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,8 @@ app_name = 'base'
 
 urlpatterns = [
     path("clients-create-view/", views.ClientCreateView.as_view(), name="clients-create-view"),
+    path("clients-list-view/", views.ClientListView.as_view(), name="clients-list-view"),
+    path("clients-detail-view/<pk>", views.ClientDetailView.as_view(), name="clients-detail-view"),
+    path("clients-update-view/<pk>", views.ClientUpdateView.as_view(), name="clients-update-view"),
+    path("clients-delete-view/<pk>", views.ClientDeleteView.as_view(), name="clients-delete-view"),
     ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,32 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from base.models import Client
 
 class ClientCreateView(CreateView):
     model = Client
     template_name = 'form.html'
     fields = "__all__"
+    success_url = reverse_lazy("homepage")
+
+
+class ClientListView(ListView):
+    template_name = "clients_list_view.html"
+    model = Client
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = "clients_detail_view.html"
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = "__all__"
+    template_name = "form.html"
+    success_url = reverse_lazy("homepage")
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = "clients_delete.html"
     success_url = reverse_lazy("homepage")
