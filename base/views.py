9f64f41bd6c0b16dde26e29d995ca6da5e3d1290
Szuwar <marekszubartowicz@gmail.com>
2022-07-09 08:58:30 +0200

    add login request

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView, FormView
 
@@ -9,7 +11,7 @@ from base.models import Client, Address
 #zmienić na FormView i przypisać adres do klienta
 
 
-
+@login_required
 def client_address_create(request):
     form = ClientForm(request.POST or None)
     form_2 = AddressForm(request.POST or None)
@@ -32,38 +34,59 @@ def client_address_create(request):
                      "form_2": form_2})
 
 
-class ClientListView(ListView):
+class ClientListView(LoginRequiredMixin,ListView):
     template_name = "clients_list_view.html"
     model = Client
 
 
-class ClientDetailView(DetailView):
+class ClientDetailView(LoginRequiredMixin,DetailView):
     model = Client
     template_name = "clients_detail_view.html"
 
 
-class ClientUpdateView(UpdateView):
+class ClientUpdateView(LoginRequiredMixin,UpdateView):
     model = Client
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
 
-class ClientDeleteView(DeleteView):
+class ClientDeleteView(LoginRequiredMixin,DeleteView):
     model = Client
     template_name = "clients_delete.html"
     success_url = reverse_lazy("homepage")
 
 # WIDOKI RECYCLER
 
-class RecyclerModelFormView(FormView):
+class RecyclerModelFormView(LoginRequiredMixin,FormView):
     template_name = "form.html"
     form_class = RecyclerModelForm
     success_url = reverse_lazy("homepage")
 
 # WIDOKI ORDERS
 
-class OrderModelFormView(FormView):
+class OrderModelFormView(LoginRequiredMixin,FormView):
     template_name = "form.html"
     form_class = OrderModelForm
     success_url = reverse_lazy("homepage")
+
+@login_required
+def order_user(request):
+    if request.method == "POST":
+        form = OrderModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            trash = form.cleaned_data["trash"]
+            if trash == "EW":
+                return redirect("/trash/ewastes-create-view/")
+            if trash == "RW":
+                return redirect("/trash/rwastes-create-view/")
+            if trash == "HW":
+                return redirect("/trash/hwastes-create-view/")
+            if trash == "LSW":
+                return redirect("/trash/lswastes-create-view/")
+
+    else:
+        form = OrderModelForm
+    return render(request, 'form.html', {"form":form})
+
--- trash/views.py
+++ trash/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
@@ -5,127 +6,127 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView,
 from trash.models import Trash, EWaste, RWaste, HWaste, LSWaste
 
 
-class TrashListView(ListView):
+class TrashListView(LoginRequiredMixin, ListView):
     template_name = "list_view.html"
     model = Trash
 
-class TrashCreateView(CreateView):
+class TrashCreateView(LoginRequiredMixin,CreateView):
     model = Trash
     fields = "__all__"
     template_name = "form.html"
-    success_url = reverse_lazy("homepage")
+    success_url = reverse_lazy("trash:ewastes-create-view")
 
-class TrashDetailView(DetailView):
+class TrashDetailView(LoginRequiredMixin, DetailView):
     model = Trash
     template_name = "my_trashes.html"
 
-class TrashUpdateView(UpdateView):
+class TrashUpdateView(LoginRequiredMixin, UpdateView):
     model = Trash
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
-class TrashDeleteView(DeleteView):
+class TrashDeleteView(LoginRequiredMixin, DeleteView):
     model = Trash
     template_name = "delete_trash.html"
     success_url = reverse_lazy("homepage")
 
-class EWasteListView(ListView):
+class EWasteListView(LoginRequiredMixin, ListView):
     template_name = "list_view.html"
     model = EWaste
 
-class EWasteCreateView(CreateView):
+class EWasteCreateView(LoginRequiredMixin, CreateView):
     model = EWaste
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
-class EWasteDetailView(DetailView):
+class EWasteDetailView(LoginRequiredMixin, DetailView):
     model = EWaste
     template_name = "my_ewastes.html"
 
-class EWasteUpdateView(UpdateView):
+class EWasteUpdateView(LoginRequiredMixin, UpdateView):
     model = EWaste
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
-class EWasteDeleteView(DeleteView):
+class EWasteDeleteView(LoginRequiredMixin, DeleteView):
     model = EWaste
     template_name = "delete_ewaste.html"
     success_url = reverse_lazy("homepage")
 
-class RWasteListView(ListView):
+class RWasteListView(LoginRequiredMixin, ListView):
     template_name = "list_view.html"
     model = RWaste
 
-class RWasteCreateView(CreateView):
+class RWasteCreateView(LoginRequiredMixin, CreateView):
     model = RWaste
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
-class RWasteDetailView(DetailView):
+class RWasteDetailView(LoginRequiredMixin, DetailView):
     model = RWaste
     template_name = "my_rwastes.html"
 
-class RWasteUpdateView(UpdateView):
+class RWasteUpdateView(LoginRequiredMixin, UpdateView):
     model = RWaste
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
-class RWasteDeleteView(DeleteView):
+class RWasteDeleteView(LoginRequiredMixin, DeleteView):
     model = RWaste
     template_name = "delete_rwaste.html"
     success_url = reverse_lazy("homepage")
 
-class HWasteListView(ListView):
+class HWasteListView(LoginRequiredMixin, ListView):
     template_name = "list_view.html"
     model = HWaste
 
-class HWasteCreateView(CreateView):
+class HWasteCreateView(LoginRequiredMixin, CreateView):
     model = HWaste
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
-class HWasteDetailView(DetailView):
+class HWasteDetailView(LoginRequiredMixin, DetailView):
     model = HWaste
     template_name = "my_hwastes.html"
 
-class HWasteUpdateView(UpdateView):
+class HWasteUpdateView(LoginRequiredMixin, UpdateView):
     model = HWaste
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
-class HWasteDeleteView(DeleteView):
+class HWasteDeleteView(LoginRequiredMixin, DeleteView):
     model = HWaste
     template_name = "delete_hwaste.html"
     success_url = reverse_lazy("homepage")
 
-class LSWasteListView(ListView):
+class LSWasteListView(LoginRequiredMixin, ListView):
     template_name = "list_view.html"
     model = LSWaste
 
-class LSWasteCreateView(CreateView):
+class LSWasteCreateView(LoginRequiredMixin, CreateView):
     model = LSWaste
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
-class LSWasteDetailView(DetailView):
+class LSWasteDetailView(LoginRequiredMixin, DetailView):
     model = LSWaste
     template_name = "my_lswastes.html"
 
-class LSWasteUpdateView(UpdateView):
+class LSWasteUpdateView(LoginRequiredMixin, UpdateView):
     model = LSWaste
     fields = "__all__"
     template_name = "form.html"
     success_url = reverse_lazy("homepage")
 
-class LSWasteDeleteView(DeleteView):
+class LSWasteDeleteView(LoginRequiredMixin, DeleteView):
     model = LSWaste
     template_name = "delete_lswaste.html"
     success_url = reverse_lazy("homepage")
